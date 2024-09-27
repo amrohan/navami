15f2c460c3867cdb3466131be63a240386c9609f
amrohan <rohanxsalunkhe@gmail.com>
2024-09-27 14:06:11 +0530

    feat:duplicate recipe completed

--- Dto/RecipeMasterDto.cs
+++ Dto/RecipeMasterDto.cs
@@ -31,29 +31,6 @@ namespace navami.Dto
         public List<RawMaterialUsageDto> RawMaterialUsage { get; set; } = new List<RawMaterialUsageDto>();
         public List<RecipeCategoryMappingDto> RecipeCategory { get; set; } = new List<RecipeCategoryMappingDto>();
 
-        //public int? RecipeId { get; set; }
-        //public string? RecipeCode { get; set; }
-        //public string RecipeName { get; set; } = null!;
-        //public string? Profile { get; set; }
-        //public string Username { get; set; } = null!;
-        //public string? RecipeCategories { get; set; }
-        //public decimal AdjustedCost { get; set; }
-
-        //public decimal TotalCost { get; set; }
-
-        //public bool IsActive { get; set; }
-
-        //public Guid CreatedBy { get; set; }
-
-        //public DateTime CreatedAt { get; set; }
-
-        //public Guid? UpdatedBy { get; set; }
-
-        //public DateTime? UpdatedAt { get; set; }
-
-        //// custom properties
-
-
     }
 
 }
